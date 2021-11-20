d9c153d64317a730c7955e64fc8977fcf5f8ee71
Dario Cubillos <dariocubillos1@gmail.com>
2021-11-20 16:59:33 -0600

    End problem 1

--- Problem1/Problem1.ts
+++ Problem1/Problem1.ts
@@ -1,15 +1,31 @@
 interface BintreeNode {
-    id: string | number,
-    left: BintreeNode,
-    right: BintreeNode
+    id?: string | number,
+    left?: BintreeNode,
+    right?: BintreeNode
 }
 
+let result:BintreeNode = {};
 let input = ["a", ["b"], ["c"]];
+let input2 = [1, [2], [3, null,[5]]];
 
-
-function convertion(data:Array<any>):void {
-
+function convertion(result:BintreeNode, data:Array<any>):BintreeNode {
+    for (const key in data) {
+        if (Object.prototype.hasOwnProperty.call(data, key)) { 
+            const index:number = +key;
+            switch (index) {
+                case 0:
+                    result.id = data[key];
+                    break;
+                case 1:
+                    result.left = Array.isArray(data[key]) ? convertion({},data[key]) : data[key];
+                    break;
+                case 2:
+                    result.right =  Array.isArray(data[key]) ? convertion({},data[key]) : data[key];
+                    break;
+            }
+        }
+    }
+    return result;
 }
 
-
-convertion(input);
+console.log(JSON.stringify(convertion(result,input2)));
