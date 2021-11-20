8bcf29f2c6d7d87d2367774647b1a652698f42c3
dariocubillos <dariocubillos1@gmail.com>
2021-11-20 13:07:57 -0600

    update and create initial code

---
+++ Problem1/Problem1.ts
@@ -0,0 +1,15 @@
+interface BintreeNode {
+    id: string | number,
+    left: BintreeNode,
+    right: BintreeNode
+}
+
+let input = ["a", ["b"], ["c"]];
+
+
+function convertion(data:Array<any>):void {
+
+}
+
+
+convertion(input);
